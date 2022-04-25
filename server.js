809ee11e1409dad64e6dbf3b5150c0c915454555
Gravity <tevoned@gmail.com>
2022-04-25 00:29:25 -0400

    comment feature

--- server.js
+++ server.js
@@ -26,3 +26,15 @@ const helpers = require('/utils/helpers');
 
 const hbs = exphbs.create({ helpers });
 
+app.engine('handlebars', hbs.engine);
+app,set('view engine', 'handlebars');
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(require('./controllers/'));
+
+sequelize.sync({ forse: false }).then(() => {
+    app.listen(PORT, () => console.log('NOW listening'));
+});
