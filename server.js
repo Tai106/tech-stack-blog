96f9145fdb24813a21c5ffd44fd4f34160cda927
Gravity <tevoned@gmail.com>
2022-04-28 15:54:50 -0400

    make it better

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const exphbs = require('express-handlebars');
 
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3001;
 
 const sequelize = require("./config/connection");
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
