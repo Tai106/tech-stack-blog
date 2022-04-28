ad28cc5a70f019bb0ab1758d39af7ac4d866df1a
Gravity <tevoned@gmail.com>
2022-04-27 22:05:35 -0400

    make it better

--- utils/helpers.js
+++ utils/helpers.js
@@ -7,7 +7,8 @@ module.exports = {
     format_plural: (word, amount) => {
         if (amount !== 1) {
             return `${word}s`;
-        }
+        }  
+        
         return word;
     }
 };
