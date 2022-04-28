44324345f96bbf3f381eccdba2bb233e5fe02f7d
Gravity <tevoned@gmail.com>
2022-04-27 22:41:18 -0400

    test

--- seeds/user-seeds.js
+++ seeds/user-seeds.js
@@ -3,21 +3,6 @@ const { User, Post } = require('../models');
 
 const userdata = [
     {
-        username: 'alesmonde0',
-        email: 'nwestnedge0@cbc.ca',
-        password: 'password123'
-    },
-    {
-        username: 'jwilloughway1',
-        email: 'rmebes1@sogou.com',
-        password: 'password1234'
-    },
-    {
-        username: 'iboddam2',
-        email: 'cstoneman2@last.fm',
-        password: 'password1235'
-    },
-    {
     username: 'tai106',
     email: 'itsme@gotcha.com',
     password: 'password12356'
