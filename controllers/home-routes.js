850e5ffecbb58d77d2884e4ce23b528a6e31154e
Gravity <tevoned@gmail.com>
2022-04-30 22:45:52 -0400

    updates

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -2,6 +2,7 @@ const router = require('express').Router();
 const sequelize = require('../config/connection');
 const { Post, User, Comment } = require('../models');
 
+// get all posts for homepage
 router.get('/', (req, res) => {
     console.log('======================');
     Post.findAll({
@@ -40,6 +41,7 @@ router.get('/', (req, res) => {
         });
 });
 
+// get single post
 router.get('/post/:id', (req, res) => {
     Post.findOne({
         where: {
